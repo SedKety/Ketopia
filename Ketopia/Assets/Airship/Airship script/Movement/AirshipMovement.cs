16d5535515dbe20d34e44a4c1c541059264e33af
SedKety <nobe.mol@gmail.com>
2024-04-24 13:00:20 +0200

    Kleine edits
    scenes gepusht

--- Ketopia/Assets/Airship/Airship script/Movement/AirshipMovement.cs
+++ Ketopia/Assets/Airship/Airship script/Movement/AirshipMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class AirshipMovement : MonoBehaviour
 {
-    public int airshipMovementSpeed;
+    public float airshipMovementSpeed;
+    public float airshipMovementSpeedMultiplier;
+    public float airshipRotationSpeed;
+
+
     public bool airshipMovementEnabled;
+
     public Rigidbody rb;
+
     public static AirshipMovement _instance;
 
     private void Start()
@@ -16,6 +22,21 @@ public class AirshipMovement : MonoBehaviour
         {
             _instance = this;
         }
-         rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+    public void FixedUpdate()
+    {
+        if (airshipMovementEnabled)
+        {
+            var rotationValue = Input.GetAxis("Horizontal");
+            rotationValue = rotationValue * airshipRotationSpeed;
+            gameObject.transform.Rotate(0, rotationValue, 0);
+
+
+            var movementValue = Input.GetAxis("Vertical");
+            movementValue = movementValue * airshipMovementSpeed * airshipMovementSpeedMultiplier;
+
+            transform.Translate(0, movementValue, 0);
+        }
     }
 }
