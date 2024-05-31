7d640da2da3eca0084348c4fda8f9ed6fed01211
Omenio <hvr123uddel@gmail.com>
2024-05-31 14:37:13 +0200

    meer moderls en sound

---
+++ Ketopia/Assets/UI en UX/SoundSlider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSlider : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private Text volumeTextUI = null;
+    void Start()
+    {
+        LoadValues();
+    }
+
+    public void VolumeSlider(float volume)
+    {
+        volumeTextUI.text = volume.ToString("0.0");
+    }
+    public void saveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+}
