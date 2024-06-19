53ac03542690212a01a70cdbcf333ffc5dcccf47
SedKety <nobe.mol@gmail.com>
2024-06-19 21:36:07 +0200

    Je kan nu een build maken

--- Ketopia/Assets/Nobe/CustomEditors/AirshipMovementEnablerEditor.cs
+++ Ketopia/Assets/Nobe/CustomEditors/AirshipMovementEnablerEditor.cs
@@ -1,21 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(AirshipMovement))]
-public class AirshipMovementEnableEditor : Editor
+public class AirshipMovementEnableEditor
 {
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        AirshipMovement movement = (AirshipMovement)target;
 
-        if (GUILayout.Button("EnableMovement"))
-        {
-            movement.EnableMovement();
-        }
-        if (GUILayout.Button("DisableMovement"))
-        {
-            movement.DisableMovement();
-        }
-    }
 }
